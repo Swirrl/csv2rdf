4c6a1bb6296fba98840f5db9272693a299a9b6e4
lkitching <lee@swirrl.com>
2018-02-13 17:53:58 +0000

    Generate descriptions of RDF test cases in the CSVW test suite.
    Parse the manifest.csv file and generate a data structure representing
    each RDF test case in the CSVW test suite. There are two types of test:
    those that start from the metadata file and those that start from the
    URI of the CSV file.

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "https://github.com/Swirrl/csv2rdf"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.csv "0.1.4"]]
+  :profiles
+  {:dev
+   {:resource-paths ["test/resources"]}})
---
+++ test/csv2rdf/csvw_test.clj
@@ -0,0 +1,195 @@
+(ns csv2rdf.csvw-test
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
+            [clojure.string :as string])
+  (:import [java.net URI]))
+
+(def test-data-dir (io/file "csvw_data"))
+(def test-base-uri (URI. "http://www.w3.org/2013/csvw/tests/"))
+(def manifest (io/file test-data-dir "manifest.csv"))
+
+(defn test-path->file [path]
+  (io/file test-data-dir path))
+
+(defn test-path->uri [path]
+  (.resolve test-base-uri path))
+
+(defn get-file-extension [file]
+  (let [filename (.getName file)
+        idx (.lastIndexOf filename ".")]
+    (if (= -1 idx)
+      ""
+      (.substring filename (inc idx)))))
+
+(defn read-manifest [manifest-file]
+  (with-open [r (io/reader manifest-file)]
+    (doall (csv/read-csv r))))
+
+(defn is-nil-or-whitespace? [s]
+  (or (nil? s)
+      (.. s trim isEmpty)))
+
+(defn strip-quotes [s]
+  (if-let [match (re-find #"^\"(.*)\"$" s)]
+    (second match)
+    s))
+
+(defn parse-extras [extras]
+  (if (is-nil-or-whitespace? extras)
+    {}
+    (let [kvs (string/split extras #"\s+")
+          kvs (map (fn [p]
+                     (let [[k v & ignored] (string/split p #"=")
+                           v (strip-quotes v)
+                           v (if (.contains v ",") (string/split v #",") v)]
+                       [(keyword k) v]))
+                   kvs)]
+      (into {} kvs))))
+
+(defn ->coll [x]
+  (if (coll? x) x [x]))
+
+(defn parse-row
+  "Parses a single row in manifest.csv from the CSVW test cases repository. See mk_manifest.rb in the test data repository
+   for how each row is structured"
+  [{:strs [test name comment approval rdf json validate action-metadata user-metadata link-metadata extra] :as row}]
+  (let [extras (parse-extras extra)
+        is-rdf-test? (not= "FALSE" rdf)
+        is-json-test? (not= "FALSE" json)
+        has-rdf-result? (not (contains? #{"FALSE" "negative"} rdf))
+        has-json-result? (not (contains? #{"FALSE" "negative"} json))
+        extras (if-not (= "FALSE" rdf) (assoc extras :rdf rdf) extras)
+        extras (if-not (= "FALSE" json) (assoc extras :json json) extras)
+        extras (if-not (= "FALSE" validate) (assoc extras :validation validate) extras)
+        has-dir? (contains? extras :dir)
+        extra-action? (contains? extras :action)
+        m (if has-dir?
+            (let [action (cond
+                           extra-action? (:action extras)
+                           (= "TRUE" action-metadata) (str test "/csv-metadata.json")
+                           :else (str test "/action.csv"))]
+              {:action     action
+               :result-rdf (if has-rdf-result? (str test "/result.ttl"))
+               :result-json (if has-json-result? (str test "/result.json"))
+               :user-metadata (if (= "TRUE" user-metadata) (str test "/user-metadata.json"))
+               :link-metadata (if (= "TRUE" link-metadata) (str test "/linked-metadata.json"))})
+            (let [action (cond
+                           extra-action? (:action extras)
+                           (= "TRUE" action-metadata) (str test "-metadata.json")
+                           :else (str test ".csv"))]
+              {:action action
+               :result-rdf (if has-rdf-result? (str test ".ttl"))
+               :result-json (if has-json-result? (str test ".json"))
+               :user-metadata (if (= "TRUE" user-metadata) (str test "-user-metadata.json"))
+               :link-metadata (if (= "TRUE" link-metadata) (str test "-linked-metadata.json"))}))
+        implicit (cond
+                   (contains? extras :implicit)
+                   (->coll (:implicit extras))
+
+                   (and (not extra-action?)
+                        (= "TRUE" action-metadata))
+                   [(if has-dir?
+                      (str test "/action.csv")
+                      (str test ".csv"))]
+                   :else [])
+        implicit (->> implicit
+                      (concat [(:user-metadata m)
+                               (:link-metadata m)])
+                      (remove nil?)
+                      (vec))
+        http-link (if-let [link-meta (:link-metadata m)]
+                    (format "<%s>; rel=\"describedby\"; type=\"application/csvm+json\"" (.getName (io/file link-meta))))]
+    (merge
+      m
+      {:id test
+       :name name
+       :comment comment
+       :approval approval
+       :extra extras
+       :implicit implicit
+       :http-link http-link})))
+
+(defn parse-manifest
+  ([] (parse-manifest manifest))
+  ([manifest-file]
+   (let [[header & rows] (read-manifest manifest-file)
+         row-maps (map (fn [row] (zipmap header row)) rows)]
+     (map parse-row row-maps))))
+
+(defn file->request [file]
+  (let [result-file (test-path->file file)
+        content-type (if (= "csv" (get-file-extension result-file)) "text/csv" "application/csvm+json")]
+    {:body    result-file
+     :uri     (test-path->uri file)
+     :headers {"Content-Type" content-type}}))
+
+(defn minimal-mode? [test-row]
+  (= "true" (get-in test-row [:extra :minimal])))
+
+(defn from-metadata-test-descriptor
+  "Tests that start from a metadata file use it to locate the CSV file (which always exists?). Any additional files
+   required by the test should appear in the list of implicit files"
+  [{:keys [action implicit] :as test-row}]
+  {:type :metadata
+   :metadata-file (test-path->file action)
+   :schema-path action
+   :requests (mapv file->request implicit)})
+
+(def well-known-metadata-file (io/resource "csvm"))
+
+(def well-known-metadata-response
+  {:uri (URI. "http://www.w3.org/.well-known/csvm")
+   :body well-known-metadata-file
+   :headers {}})
+
+(defn has-well-known-metadata? [test-row]
+  (.contains (:name test-row) "/.well-known/csvm"))
+
+(defn from-csv-test-descriptor
+  "Tests that start from the URI of the CSV file. These always contain a request for the CSV file itself, and may include
+   one for the well-known metadata file and any implicit files."
+  [{:keys [action implicit http-link user-metadata] :as test-row}]
+  (let [action-file (test-path->file action)
+        action-uri (test-path->uri action)
+        action-headers {"Content-Type" "text/csv; charset=UTF-8"}
+        action-headers (if (some? http-link)
+                         (assoc action-headers "Link" http-link)
+                         action-headers)
+        action-request {:body action-file
+                        :uri action-uri
+                        :headers action-headers}
+        metadata-requests (if (has-well-known-metadata? test-row)
+                             [well-known-metadata-response]
+                             [])]
+    {:type        :csv
+     :action-uri  action-uri
+     :schema-path user-metadata
+     :requests    (vec (cons action-request (concat (map file->request implicit) metadata-requests)))}))
+
+(defn rdf-test-type [test-row]
+  (let [type (some-> (get-in test-row [:extra :rdf]) keyword)]
+    (#{:positive :negative :warning} type)))
+
+(defn test-descriptor [{:keys [action result-rdf] :as test-row}]
+  (let [action-file (test-path->file action)
+        action-ext (get-file-extension action-file)
+        from-metadata? (= "json" action-ext)
+        test-type (rdf-test-type test-row)]
+    ;;ignore any non-rdf tests
+    (if (some? test-type)
+      (let [m (if from-metadata?
+                (from-metadata-test-descriptor test-row)
+                (from-csv-test-descriptor test-row))]
+        (merge
+          m
+          {:minimal? (minimal-mode? test-row)
+           :expect-errors? (= :negative test-type)
+           :expect-warnings? (= :warning test-type)
+           :result-file (some-> result-rdf (test-path->file))})))))
+
+(defn get-tests []
+  (->> (parse-manifest)
+       (filter #(> (.length (:id %)) 0))  ;;test026 appears to be invalid? Has no name and mising result file
+       (map test-descriptor)
+       (remove nil?)))
+
