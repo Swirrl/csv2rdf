c9140c2d08624c541ef40182c436fd96804b5655
lkitching <lee@swirrl.com>
2018-03-02 10:09:36 +0000

    Implement length calculation for string types.
    Represent the base type hierarchy directly so subtype relationships can
    be determined. Implement length for string subtypes according to the
    specification.

--- src/csv2rdf/metadata/datatype.clj
+++ src/csv2rdf/metadata/datatype.clj
@@ -1,12 +1,68 @@
 (ns csv2rdf.metadata.datatype
   (:require [clojure.spec.alpha :as s]))
 
-(s/def ::base #{"anyAtomicType" "anyURI" "base64Binary" "boolean" "date" "dateTime" "dateTimeStamp"
-                "decimal" "integer" "long" "int" "short" "byte" "nonNegativeInteger" "positiveInteger"
-                "unsignedLong" "unsignedInt" "unsignedShort" "unsignedByte" "nonPositiveInteger"
-                "negativeInteger" "double" "duration" "dayTimeDuration" "yearMonthDuration" "float"
-                "gDay" "gMonth" "gMonthDay" "gYear" "gYearMonth" "hexBinary" "QName" "string"
-                "normalizedString" "token" "language" "Name" "NMTOKEN" "xml" "html" "json" "time"})
+(def type-hierarchy
+  ["anyAtomicType"
+   ["anyURI"
+    "base64Binary"
+    "boolean"
+    ["date" ["dateTime"]]
+    ["decimal" [["integer"
+                 [["long"
+                   [["int"
+                     [["short"
+                       ["byte"]]]]]]]]
+                ["nonNegativeInteger"
+                 ["positiveInteger"
+                  ["unsignedLong"
+                   [["unsignedInt"
+                     [["unsignedShort"
+                       ["unsignedByte"]]]]]]]]
+                ["nonPositiveInteger" ["negativeInteger"]]]]
+    "double"
+    ["duration" ["dayTimeDuration" "yearMonthDuration"]]
+    "float"
+    "gDay"
+    "gYear"
+    "gMonth"
+    "hexBinary"
+    "QName"
+    ["string"
+     [["normalizedString" [["token" ["language" "Name" "NMTOKEN"]]]]
+      "xml"
+      "html"
+      "json"]]
+    "time"]])
+
+(defn is-leaf? [x] (string? x))
+(defn is-inner? [x] (and (vector? x) (= 2 (count x))))
+(defn children [node]
+  (if (is-leaf? node) nil (second node)))
+(defn node-name [x]
+  (if (is-leaf? x) x (first x)))
+
+(defn find-root
+  ([type-name] (find-root type-name type-hierarchy))
+  ([type-name node]
+   (loop [q [node]]
+     (if-let [node (first q)]
+       (if (= type-name (node-name node))
+         node
+         (recur (concat (rest q) (children node))))))))
+
+(defn node-subtypes [node]
+  (if (is-leaf? node)
+    [node]
+    (cons (node-name node) (mapcat node-subtypes (children node)))))
+
+(defn subtypes [type-name]
+  (if-let [root (find-root type-name)]
+    (node-subtypes root)))
+
+(defn is-subtype? [supertype-name type-name]
+  (boolean (some #(= type-name %) (subtypes supertype-name))))
+
+(s/def ::base  (into #{} (flatten type-hierarchy)))
 
 (s/def ::num-or-string (s/or :string string? :num number?))
 
@@ -32,3 +88,18 @@
   (merge-with #(or %1 %2) dt default))
 
 (s/fdef expand :ret ::datatype)
+
+(defn is-binary-type? [datatype]
+  ;;TODO: figure out what this means
+  )
+
+(defn binary-length [value datatype]
+  (throw (IllegalStateException. "Should not be called until is-binary-type? is implemented")))
+
+(defn ^{:table-spec "4.6.1"} get-length [value {:keys [base] :as datatype}]
+  (cond
+    (nil? value) 0
+    (is-subtype? "string" base) (.count (.codePoints value))
+    (is-binary-type? datatype) (binary-length value datatype)
+    :else (throw (IllegalStateException. "TODO: return 0?"))
+    ))
