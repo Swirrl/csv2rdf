f63403864de9a6ac2f004cc22a61cb5cbfbb588f
lkitching <lee@swirrl.com>
2018-03-16 17:53:03 +0000

    Validate schema column reference properties are all valid.

--- src/csv2rdf/metadata.clj
+++ src/csv2rdf/metadata.clj
@@ -559,14 +559,45 @@
     {:required {"columnReference" column-reference
                 "reference"       (object-property foreign-key-reference)}}))
 
+(defn validate-column-references
+  "Validates all the referenced column names under the parent key exist in the given set of column names defined
+   by the schema. If the reference column names do not appear directly under the parent key, child-column-name-fn
+   should return all ancestor column names referenced within the value associated with key-name. If any referenced
+   columns do not exist, a warning is added and the value associated with key-name is removed from the schema as
+   though it was not specified."
+  ([key-name column-names] (validate-column-references key-name column-names identity))
+  ([key-name column-names child-column-name-fn]
+   (fn [context schema]
+     (if (contains? schema key-name)
+       (let [specified-columns (child-column-name-fn (get schema key-name))
+             invalid-columns (remove (fn [cn] (contains? column-names cn)) specified-columns)]
+         (if (seq invalid-columns)
+           (let [msg (format "Referenced column names: %s do not exist in the list of columns" (string/join ", " invalid-columns))]
+             (make-warning (append-path context (name key-name)) msg (dissoc schema key-name)))
+           (v/pure schema)))
+       (v/pure schema)))))
+
+(defn validate-schema-column-references
+  "Validator which checks all column name properties within the schema reference columns defined in the columns collection.
+   Any components which reference non-existent columns are removed."
+  [context {:keys [columns] :as schema}]
+  (let [column-names (into #{} (map :name columns))
+        validator (chain
+                    (validate-column-references :primaryKey column-names)
+                    (validate-column-references :rowTitles column-names)
+                    (validate-column-references :foreignKeys column-names (fn [foreign-keys] (mapcat :columnReference foreign-keys))))]
+    (validator context schema)))
+
 (def schema
-  (metadata-of
-    {:optional {"columns" columns
-                "foreignKeys" (array-of foreign-key)        ;;TODO: validate foreign keys
-                "primaryKey" column-reference               ;;TODO: validators MUST check that each row has a unique combination of values of cells in the indicated columns
-                "rowTitles" column-reference
-                "@id" id
-                "@type" (eq "Schema")}}))
+  (chain
+    (metadata-of
+      {:optional {"columns"     columns
+                  "foreignKeys" (array-of foreign-key)      ;;TODO: validate foreign keys
+                  "primaryKey"  column-reference            ;;TODO: validators MUST check that each row has a unique combination of values of cells in the indicated columns
+                  "rowTitles"   column-reference
+                  "@id"         id
+                  "@type"       (eq "Schema")}})
+    validate-schema-column-references))
 
 (def table
   (metadata-of
