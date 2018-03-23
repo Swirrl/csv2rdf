9fda4a07251b328c4967f403ed7a6a1e3f78a9e4
lkitching <lee@swirrl.com>
2018-03-23 16:55:27 +0000

    Resolve URI template properties as described in the specification.

---
+++ src/csv2rdf/metadata/uri_template_property.clj
@@ -0,0 +1,21 @@
+(ns csv2rdf.metadata.uri-template-property
+  (:require [clojure.spec.alpha :as s]
+            [csv2rdf.uri-template :as template]
+            [csv2rdf.json-ld :as json-ld]
+            [csv2rdf.util :as util])
+  (:import (java.net URI)))
+
+(s/def ::_column pos?)
+(s/def ::_sourceColumn pos?)
+(s/def ::_row pos?)
+(s/def ::_name string?)
+
+(s/def ::column-bindings (s/keys :req-un [::_column ::_sourceColumn ::_row ::_name]))
+
+(defn ^{:metadata-spec "5.1.3"} resolve-uri-template-property [uri-template column-bindings {:keys [url] :as table}]
+  ;;TODO: expanded template strings are percent encoded. See if there's a way to avoid the immediate decoding
+  (let [resolved-uri-string (util/percent-decode (template/expand-template-to-string uri-template column-bindings))
+        expanded-uri-string (json-ld/expand-uri-string resolved-uri-string)]
+    (if (nil? url)
+      (URI. expanded-uri-string)
+      (.resolve url expanded-uri-string))))
--- src/csv2rdf/uri_template.clj
+++ src/csv2rdf/uri_template.clj
@@ -5,7 +5,11 @@
 
 (defn make-variable-map [m]
   (let [builder (reduce (fn [b [k v]]
-                          (.addScalarValue b (name k) v))
+                          (let [var-name (if (keyword? k) (name k) k)]
+                            (cond
+                              (map? v) (.addMapValue b var-name v)
+                              (coll? v) (.addListValue b var-name v)
+                              :else (.addScalarValue b var-name v))))
                         (VariableMap/newBuilder)
                         m)]
     (.freeze builder)))
@@ -19,6 +23,9 @@
 
 (defn expand-template [template vars]
   (.toURI template (make-variable-map vars)))
+
+(defn expand-template-to-string [template vars]
+  (.toString template (make-variable-map vars)))
 
 (defn expand-template-string [template-string vars]
   (expand-template (parse-template template-string) vars))
