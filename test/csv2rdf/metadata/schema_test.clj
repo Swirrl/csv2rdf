f93b37defe011c11ed0b29c16294dc1cedde6612
lkitching <lee@swirrl.com>
2018-06-04 17:02:08 +0100

    Update metadata.schema-test.
    Update metadata.schema-test to use validates-as, warns-with and
    warns-invalid.

--- test/csv2rdf/metadata/schema_test.clj
+++ test/csv2rdf/metadata/schema_test.clj
@@ -1,27 +1,24 @@
 (ns csv2rdf.metadata.schema-test
   (:require [clojure.test :refer :all]
             [csv2rdf.metadata.schema :refer :all]
-            [csv2rdf.metadata.test-common :refer [test-context validation-error]]
-            [csv2rdf.metadata.validator :refer [invalid?]]
-            [csv2rdf.logging :as logging])
+            [csv2rdf.metadata.test-common :refer [test-context validation-error validates-as warns-with warns-invalid]]
+            [csv2rdf.metadata.validator :refer [invalid?]])
   (:import [java.net URI]))
 
 (deftest foreign-key-reference-test
   (testing "Valid definition with resource"
     (let [resource-uri-str "http://resource/table"
           fk {"columnReference" "col1"
-              "resource" resource-uri-str}
-          {:keys [warnings result]} (logging/capture-warnings (foreign-key-reference test-context fk))]
-      (is (empty? warnings))
-      (is (= {:columnReference ["col1"] :resource (URI. resource-uri-str)} result))))
+              "resource" resource-uri-str}]
+      (validates-as {:columnReference ["col1"] :resource (URI. resource-uri-str)}
+                    (foreign-key-reference test-context fk))))
 
   (testing "Valid definition with schemaReference"
     (let [schema-uri-str "http://schema/id"
           fk {"columnReference" ["col1" "col4"]
-              "schemaReference" schema-uri-str}
-          {:keys [warnings result]} (logging/capture-warnings (foreign-key-reference test-context fk))]
-      (is (empty? warnings))
-      (is (= {:columnReference ["col1" "col4"] :schemaReference (URI. schema-uri-str)} result))))
+              "schemaReference" schema-uri-str}]
+      (validates-as {:columnReference ["col1" "col4"] :schemaReference (URI. schema-uri-str)}
+                    (foreign-key-reference test-context fk))))
 
   (testing "Invalid contain no columnReference"
     (validation-error (foreign-key-reference test-context {"resource" "http://table"})))
@@ -46,13 +43,12 @@
   (testing "Valid"
     (let [fk {"columnReference" "keyCol"
               "reference" {"columnReference" "refCol"
-                           "resource" "http://table"}}
-          {:keys [warnings result]} (logging/capture-warnings (foreign-key test-context fk))]
-      (is (empty? warnings))
-      (is (= {:columnReference ["keyCol"]
-              :reference {:columnReference ["refCol"]
-                          :resource (URI. "http://table")}}
-             result))))
+                           "resource" "http://table"}}]
+      (validates-as
+        {:columnReference ["keyCol"]
+         :reference {:columnReference ["refCol"]
+                     :resource (URI. "http://table")}}
+        (foreign-key test-context fk))))
 
   (testing "Invalid no column reference"
     (validation-error (foreign-key test-context {"reference" {"columnReference" "col1" "resource" "http://table"}})))
@@ -61,9 +57,7 @@
     (validation-error (foreign-key test-context {"columnReference" "col1" "reference" "not an object"})))
 
   (testing "Invalid type"
-    (let [{:keys [warnings result]} (logging/capture-warnings (foreign-key test-context "not an object"))]
-      (is (= 1 (count warnings)))
-      (is (invalid? result))))
+    (warns-invalid (foreign-key test-context "not an object")))
 
   (testing "Contains extra keys"
     (validation-error (foreign-key test-context {"columnReference" "col"
@@ -79,24 +73,19 @@
                     :rowTitles   ["col1" "col2"]
                     :foreignKeys [{:columnReference ["col2"]
                                    :reference       {:columnReference ["refCol"]
-                                                     :resource        (URI. "http://table")}}]}
-            {:keys [warnings result]} (logging/capture-warnings (validate-schema-column-references test-context schema))]
-        (is (empty? warnings))
-        (is (= schema result))))
+                                                     :resource        (URI. "http://table")}}]}]
+        (validates-as schema (validate-schema-column-references test-context schema))))
 
     (testing "Invalid primary key"
       (let [schema {:columns    columns
-                    :primaryKey ["col1" "missing"]}
-            {:keys [warnings result]} (logging/capture-warnings (validate-schema-column-references test-context schema))]
-        (is (= 1 (count warnings)))
-        (is (= {:columns columns} result))))
+                    :primaryKey ["col1" "missing"]}]
+        (warns-with {:columns columns}
+                    (validate-schema-column-references test-context schema))))
 
     (testing "Invalid row titles"
       (let [schema {:columns columns
-                    :rowTitles ["missing" "bad column"]}
-            {:keys [warnings result]} (logging/capture-warnings (validate-schema-column-references test-context schema))]
-        (is (= 1 (count warnings)))
-        (is (= {:columns columns} result))))
+                    :rowTitles ["missing" "bad column"]}]
+        (warns-with {:columns columns} (validate-schema-column-references test-context schema))))
 
     (testing "Invalid foreign key"
       (let [schema {:columns columns
