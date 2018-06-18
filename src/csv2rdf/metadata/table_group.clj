64e983921e6ec34e1d8f7f52107598002e4d22ff
lkitching <lee@swirrl.com>
2018-06-18 12:51:41 +0100

    Validate declared foreign key references.
    Issues #10 and #11 - Validate that the referenced table/schema and
    columns exist for every foreign key reference.

--- src/csv2rdf/metadata/table_group.clj
+++ src/csv2rdf/metadata/table_group.clj
@@ -6,7 +6,8 @@
             [csv2rdf.metadata.schema :as schema]
             [csv2rdf.metadata.transformation :as transformation]
             [csv2rdf.metadata.dialect :as dialect]
-            [csv2rdf.metadata.table :as table]))
+            [csv2rdf.metadata.table :as table]
+            [csv2rdf.metadata.context :refer [append-path id-key]]))
 
 (defn tables [context x]
   (if (mjson/array? x)
@@ -16,16 +17,62 @@
         tables))
     (make-error context (type-error-message #{:array} (mjson/get-json-type x)))))
 
+(defn- find-schema-by-table-url
+  "Finds the schema for the table with the given URL in a collection of tables. Raises a validation error
+   if there is not exactly one matching table."
+  [context tables url]
+  (let [matches (into [] (filter (fn [table] (= url (:url table))) tables))]
+    (case (count matches)
+      0 (make-error context (format "Invalid table reference %s" url))
+      1 (:tableSchema (first matches))
+      (make-error context (format "Multiple tables found with url %s" url)))))
+
+(defn- find-schema-by-id
+  "Finds the schema with the given @id in a collection of table definitions. Raises a validation error
+   if there is not exactly one matching schema."
+  [context tables id]
+  (let [matches (into [] (filter (fn [schema]
+                                   (= id (get schema id-key)))
+                                 (map :tableSchema tables)))]
+    (case (count matches)
+      0 (make-error context (format "No table schema found with @id %s" id))
+      1 (first matches)
+      (make-error context (format "Multiple table schemas found with url %s" id)))))
+
+(defn validate-foreign-key-references
+  "Validates that each foreign key reference in a table group is valid by checking the referenced table/schema and
+   foreign column is declared within the group."
+  [context {:keys [tables] :as table-group}]
+  (doseq [[table-index table] (map-indexed vector tables)]
+    (when-let [foreign-keys (get-in table [:tableSchema :foreignKeys])]
+      (doseq [[fk-index fk] (map-indexed vector foreign-keys)]
+        (let [{{:keys [resource schemaReference columnReference]} :reference} fk
+              ref-schema (if (some? resource)
+                           (find-schema-by-table-url (append-path context [:tables table-index :tableSchema :foreignKeys fk-index :resource])
+                                                     tables
+                                                     resource)
+                           (find-schema-by-id (append-path context [:tables table-index :tableSchema :foreignKeys fk-index :schemaReference])
+                                              tables
+                                              schemaReference))
+              ref-schema-columns (into #{} (remove nil? (map :name (:columns ref-schema))))]
+          (doseq [[col-ref-index col-ref] (map-indexed vector columnReference)]
+            (when-not (contains? ref-schema-columns col-ref)
+              (let [col-ref-path [:tables table-index :tableSchema :foreignKeys fk-index :columnReference col-ref-index]]
+                (make-error (append-path context col-ref-path) (format "Invalid column reference '%s'" col-ref)))))))))
+  table-group)
+
 (def table-group
-  (metadata-of
-    {:required {:tables tables}
-     :optional {:dialect         (object-property dialect/dialect)
-                :notes           (array-of note)
-                :tableDirection  table-direction
-                :tableSchema     (object-property schema/schema)
-                :transformations (array-of transformation/transformation)
-                :id              id
-                :type            (type-eq "TableGroup")}}))
+  (chain
+    (metadata-of
+      {:required {:tables tables}
+       :optional {:dialect         (object-property dialect/dialect)
+                  :notes           (array-of note)
+                  :tableDirection  table-direction
+                  :tableSchema     (object-property schema/schema)
+                  :transformations (array-of transformation/transformation)
+                  :id              id
+                  :type            (type-eq "TableGroup")}})
+    validate-foreign-key-references))
 
 (defn looks-like-table-group-json? [doc]
   (contains? doc "tables"))
