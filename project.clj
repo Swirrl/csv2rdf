cc22ff89e4f1087691cd1296d0ceb28ea0e472b1
lkitching <lee@swirrl.com>
2018-06-20 11:55:59 +0100

    Version 0.2.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject csv2rdf "0.2.5-SNAPSHOT"
+(defproject csv2rdf "0.2.5"
   :description "Library for converting CSV to RDF"
   :url "https://github.com/Swirrl/csv2rdf"
   :license {:name "Eclipse Public License"
