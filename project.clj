cd030544049a03358da59b7b4f5970e3a32c35ae
lkitching <lee@swirrl.com>
2018-07-12 09:27:27 +0100

    Version 0.2.6.
    Update version to 0.2.6 and set the group id to swirrl.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject csv2rdf "0.2.6-SNAPSHOT"
+(defproject swirrl/csv2rdf "0.2.6"
   :description "Library for converting CSV to RDF"
   :url "https://github.com/Swirrl/csv2rdf"
   :license {:name "Eclipse Public License"
