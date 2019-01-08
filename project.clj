ab1bf6ef34cf5088d5fdd1a62c6796c8dbcf0d5b
lkitching <lee@swirrl.com>
2019-01-08 15:49:08 +0000

    Move dependency specification into deps.edn.
    Issue #16 - Add deps.edn containing dependencies and remove them from
    project.clj. Integrate leiningen with clojure deps through the
    lein-tools-deps plugin.

--- project.clj
+++ project.clj
@@ -3,25 +3,13 @@
   :url "https://github.com/Swirrl/csv2rdf"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/data.json "0.2.6"]
-                 [grafter "0.11.4"]
-                 [com.github.fge/uri-template "0.9"]
-                 [org.apache.httpcomponents/httpcore "4.4.9"]
-                 [clj-http "3.7.0"]
-                 [org.clojure/tools.cli "0.3.7"]
-                 [org.clojure/tools.logging "0.4.1"]
-                 [org.slf4j/slf4j-api "1.7.25"]]
+  :plugins [[lein-tools-deps "0.4.3"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
   
   :profiles
   {:uberjar {:main csv2rdf.main
-             :dependencies [[org.apache.logging.log4j/log4j-api "2.11.0"]
-                            [org.apache.logging.log4j/log4j-core "2.11.0"]
-                            [org.apache.logging.log4j/log4j-slf4j-impl "2.11.0"]]}
+             :lein-tools-deps/config {:resolve-aliases [:with-logging]}}
    :dev
-   {:dependencies [[org.clojure/test.check "0.9.0"]
-                   [org.clojure/data.csv "0.1.4"]
-                   [org.apache.logging.log4j/log4j-api "2.11.0"]
-                   [org.apache.logging.log4j/log4j-core "2.11.0"]
-                   [org.apache.logging.log4j/log4j-slf4j-impl "2.11.0"]]
+   {:lein-tools-deps/config {:resolve-aliases [:with-logging :test]}
     :resource-paths ["test/resources"]}})
