1b94c7d7de5ec71c80eebefb3cb74b3c6ebbb94e
lkitching <lee@swirrl.com>
2019-08-14 15:51:56 +0100

    Support triple quotes when parsing cells under double quote dialect.
    Issue #23 - Re-implement tabular row parsing to delegate cell parsing
    to a separate function. Implement two different cell parser functions
    for dialects which use the same esacpe and quote characters and those
    which use different characters. For dialects which use the same
    character for quotes and escapes, re-interpret the way sequences of
    quotes are handled to be different from the specification. Triple
    quotes are now treated as an opening quote followed by an escaped
    quote character rather than an escaped quote followed by an (illegal)
    opening quote.
    
    Add tests for the individual cell parser functions.

--- src/csv2rdf/tabular/csv/reader.clj
+++ src/csv2rdf/tabular/csv/reader.clj
@@ -128,50 +128,188 @@
     :end (string/trimr value)
     value))
 
-(defn ^{:tabular-spec "8"} parse-row-cells [^String row-content source-row-number {:keys [^Character escapeChar ^Character quoteChar ^Character delimiter trim-mode] :as options}]
-  (loop [idx 0
-         cells []
+(defn- skip-leading-chars
+  "Returns the first character index after the specified start index within the string s which does not
+   contain the character to-skip. The returned index will be one past the end of the string if s ends
+   without a different character being encountered."
+  [^String s index ^Character to-skip]
+  (if (and (< index (.length s))
+           (= to-skip (.charAt s index)))
+    (recur s (inc index) to-skip)
+    index))
+
+(defn parse-cell-double-quote
+  "Parses a tabular cell within a file where the escape and quote
+  character are the same, starting from a given index in a source row
+  string. Returns a map containing the raw cell contents and the
+  string index following the cell within the source string. The index
+  will be one past the end of the string if the cell is the last one
+  within the row."
+  [^String row-content source-row-number start-index {:keys [^Character quoteChar ^Character delimiter] :as options}]
+  ;;NOTE: The specification requires that when the quote and escape character are the same (i.e. a double-quote
+  ;;is used to escape quote characters), double quotes are eagerly consumed and interpreted as an escaped quote.
+  ;;This is a problem at the start of the cell since an opening quote followed by an escaped quote will be interpreted
+  ;;as an escaped quote followed by an (illegal) cell quote.
+  ;;The CSV RFC (https://tools.ietf.org/html/rfc4180) only allows escaped quotes within quoted cell values, but the csv2rdf
+  ;;specification allows escaped quotes anywhere. This is ambiguous since e.g. """"" could be either two escaped quotes or
+  ;;a single escaped quote within a quoted cell. We want to support CSV parsers that follow RFC4180 so we will resolve
+  ;;the ambiguity by:
+  ;;1. Read n leading quotes in the cell
+  ;;2. If there are an even number then treat this as (n / 2) escaped quote characters
+  ;;3. If there are an odd number then treat this as an opening quote followed by ((n - 1) / 2) escaped quote characters
+  ;;At other points in the cell, double quotes are treated as escaped quotes.
+  (let [non-quote-index (skip-leading-chars row-content start-index quoteChar)
+        quote-char-count (- non-quote-index start-index)
+        quoted (odd? quote-char-count)
+        quote-count (quot quote-char-count 2)
+        quote-prefix (String. (char-array quote-count quoteChar))]
+    ;;resume parsing from the first non-quote index
+    (loop [idx non-quote-index
+           sb (StringBuilder. quote-prefix)]
+      (if (< idx (.length row-content))
+        (let [c (.charAt row-content idx)
+              next-index (inc idx)
+              next-char (maybe-char-at row-content next-index)]
+          (cond
+            (= quoteChar c)
+            (cond
+              ;;3.1
+              (= quoteChar next-char)
+              (recur (inc next-index) (.append sb next-char))
+
+              ;;3.3.2 - peek following char if it exists, and raise an error if not the delimiter
+              quoted
+              (cond
+                ;;end of string so ok
+                (nil? next-char)
+                {:cell (.toString sb) :next-index next-index}
+
+                ;;followed by delimiter so ok
+                ;;NOTE: delimiter is not consumed
+                (= delimiter next-char)
+                {:cell (.toString sb) :next-index next-index}
+
+                ;;not end of string and not the delimiter so invalid                
+                :else
+                (throw (IllegalArgumentException. (format "Expected delimiter following quoted cell value at row %d index %d but got '%s'"
+                                                          source-row-number
+                                                          next-index
+                                                          next-char))))
+
+              ;;3.3.1 - opening quote should be at start of cell
+              ;;NOTE: leading quotes are consumed at the start of the cell so at least one non-quote character
+              ;;must have been consumed before reaching the current quote
+              :else
+              (throw (IllegalArgumentException. (format "Opening quote must be at start of cell value at row %d index %d" source-row-number idx))))          
+            
+            (= delimiter c)
+            (if quoted
+              ;;3.4.1
+              (recur (inc idx) (.append sb c))
+
+              ;;3.4.2
+              ;;TODO: return current char?
+              ;;row parser will need to look it up again!
+              ;;NOTE: delimiter is not consumed
+              {:cell (.toString sb) :next-index idx})
+
+            :else
+            (recur (inc idx) (.append sb c))))
+        (if quoted
+          (throw (IllegalArgumentException. (format "Missing closing quote on row %d for cell starting at index %d" source-row-number start-index)))
+          {:cell (.toString sb) :next-index idx})))))
+
+(defn- throw-unclosed-quote [source-row-number cell-start-index]
+  (throw (IllegalArgumentException. (format "Missing closing quote on row %d for cell starting at index %d" source-row-number cell-start-index))))
+
+(defn parse-cell-escape
+  "Parses a tabular cell within a file where the escape and quote
+  character are different, starting from a given index in a source row
+  string. Returns a map containing the raw cell contents and the
+  string index following the cell within the source string. The index
+  will be one past the end of the string if the cell is the last one
+  within the row."
+  [^String row-content source-row-number start-index {:keys [^Character escapeChar ^Character quoteChar ^Character delimiter trim-mode] :as options}]
+  (loop [idx start-index
          quoted false
          sb (StringBuilder.)]
     (if (< idx (.length row-content))
       (let [c (.charAt row-content idx)
             next-index (inc idx)
-            next-char (maybe-char-at row-content next-index)]
+            next-char (maybe-char-at row-content next-index)]        
         (cond
-          ;;3.1
-          (and (= escapeChar c) (= quoteChar next-char))
-          (recur (inc next-index) cells quoted (.append sb next-char))
-
           ;;3.2
-          (and (= escapeChar c) (not= escapeChar quoteChar))
+          (= escapeChar c)
           (if (nil? next-char)
             (throw (IllegalArgumentException. (format "Dangling escape character at row %d index %d" source-row-number idx)))
-            (recur (inc next-index) cells quoted (.append sb next-char)))
+            (recur (inc next-index) quoted (.append sb next-char)))
 
           (= quoteChar c)
           (if quoted
             ;;3.3.2 - peek following char if it exists, and raise an error if not the delimiter
-            (if (and (< (inc idx) (.length row-content))
-                     (not= delimiter (.charAt row-content (inc idx))))
-              (throw (IllegalArgumentException. (format "Expected delimiter following quoted cell value at row %d index %d" source-row-number (inc idx))))
-              (recur (inc idx) cells false sb))
+            (cond
+              ;;end of string so ok
+              (nil? next-char)
+              {:cell (.toString sb) :next-index next-index}
+
+              ;;following character is delimiter so ok
+              ;;NOTE: delimiter is not consumed
+              (= delimiter next-char)
+              {:cell (.toString sb) :next-index next-index}
+
+              :else
+              (throw (IllegalArgumentException. (format "Expected delimiter following quoted cell value at row %d index %d" source-row-number (inc idx)))))
 
             ;;3.3.1 - opening quote should be at start of cell
             (if (pos? (.length sb))
               (throw (IllegalArgumentException. (format "Opening quote must be at start of cell value at row %d index %d" source-row-number idx)))
-              (recur (inc idx) cells true sb)))
+              (recur (inc idx) true sb)))
 
           (= delimiter c)
           (if quoted
             ;;3.4.1
-            (recur (inc idx) cells quoted (.append sb c))
+            (recur (inc idx) quoted (.append sb c))
 
             ;;3.4.2
-            (recur (inc idx) (conj cells (trim-cell (.toString sb) trim-mode)) quoted (StringBuilder.)))
+            ;;NOTE: delimiter is not consumed
+            {:cell (.toString sb) :next-index idx})
 
           :else
-          (recur (inc idx) cells quoted (.append sb c))))
-      (conj cells (trim-cell (.toString sb) trim-mode)))))
+          (recur (inc idx) quoted (.append sb c))))
+
+      (if quoted
+        (throw-unclosed-quote source-row-number start-index)
+        {:cell (.toString sb) :next-index idx}))))
+
+(defn parse-row-cells
+  "Parses the cells within a single tabular row and trims them according
+  to the dialect trim mode."
+  [^String row-content source-row-number {:keys [^Character escapeChar ^Character quoteChar ^Character delimiter trim-mode] :as options}]
+  (if (zero? (.length row-content))
+      []
+      (let [;;NOTE: cells are parsed slightly differently depending on
+            ;;whether the quote and escape characters are the same or
+            ;;different. Each parser parses the next cell contents
+            ;;from the specified starting position within the string
+            ;;and returns a map containing the parsed cell contents
+            ;;and the index within the string to continue
+            ;;parsing. This should always point to the delimiter
+            ;;character or be one passed the end of the string.
+            cell-parser (if (= escapeChar quoteChar)
+                          parse-cell-double-quote
+                          parse-cell-escape)]        
+        (loop [idx 0
+               cells []]
+          (let [{:keys [cell ^long next-index]} (cell-parser row-content source-row-number idx options)]
+            ;;if there is any remaining input, next-index should refer to the delimiter
+            ;;consume it and move to the start of the next cell
+            ;;otherwise entire row has been parsed
+            ;;TODO: move quoted cell followed by delimiter validation stuff here?
+            (if (< next-index (.length row-content))
+              (let [next-char (.charAt row-content next-index)]
+                #_(assert (= delimiter next-char "Expected delimiter after parsed cell"))
+                (recur (inc next-index) (conj cells (trim-cell cell trim-mode))))
+              (conj cells (trim-cell cell trim-mode))))))))
 
 (s/def ::source-row-number (s/and integer? pos?))
 (s/def ::cells (s/coll-of string? :kind vector? :into []))
--- test/csv2rdf/tabular/csv/reader_test.clj
+++ test/csv2rdf/tabular/csv/reader_test.clj
@@ -23,6 +23,100 @@
         (int \f) "\nf"
         (int \f) "\r\nf"))))
 
+(deftest parse-cell-double-quote-test
+  (testing "Valid inputs"
+    (are [row-contents start-index expected] (= expected (parse-cell-double-quote row-contents 1 start-index {:quoteChar \' :delimiter \, :trim-mode :all}))
+      ;;No quotes to end
+      "first,second" 6 {:cell "second" :next-index 12}
+
+      ;;No quotes to delimiter
+      "first,second" 0 {:cell "first" :next-index 5}
+
+      ;;Quoted to end no escaped quotes
+      "first,'some,text'" 6 {:cell "some,text" :next-index 17}
+
+      ;;Quoted to delimiter no escaped quotes
+      "'quoted, text',second" 0 {:cell "quoted, text" :next-index 14}
+
+      ;;Unquoted with escaped quotes to end
+      "first,escaped ''quote" 6 {:cell "escaped 'quote" :next-index 21}
+
+      ;;Unquoted with escaped quotes to delimiter
+      "escaped ''quotes'',second" 0 {:cell "escaped 'quotes'" :next-index 18}
+
+      ;;Quoted with escaped quotes to end
+      "first,'''quoted'' text'" 6 {:cell "'quoted' text" :next-index 23}
+
+      ;;Quoted with escaped quotes to delimiter
+      "'''quoted'' text',second" 0 {:cell "'quoted' text" :next-index 17}
+
+      ;;Unquoted with escaped quotes to end
+      "first,''''" 6 {:cell "''" :next-index 10}
+
+      ;;Unquoted with escaped quotes to delimiter
+      "'''',second" 0 {:cell "''" :next-index 4}
+
+      ;;Returns raw cell text
+      "  first  ,second" 0 {:cell "  first  " :next-index 9}
+
+      ;;Empty cell at end of row
+      "a,b," 4 {:cell "" :next-index 4}
+      ))
+
+  (testing "Unclosed quote"
+    (is (thrown? IllegalArgumentException (parse-cell-double-quote "'Unclosed, quote :(" 1 0 {:quoteChar \' :delimiter \,}))))
+
+  (testing "Non-delimiter character after closing quote"
+    (is (thrown? IllegalArgumentException (parse-cell-double-quote "'Quoted text' followed by more stuff" 1 0 {:quoteChar \' :delimiter \,})))))
+
+(deftest parse-cell-escape-test
+  (testing "Valid inputs"
+    (are [row-contents start-index expected] (= expected (parse-cell-escape row-contents 1 start-index {:quoteChar \' :escapeChar \\ :delimiter \,}))
+      ;;No quotes to end
+      "first,second" 6 {:cell "second" :next-index 12}
+
+      ;;No quotes to delimiter
+      "first,second" 0 {:cell "first" :next-index 5}
+
+      ;;Quoted to end no escaped quotes
+      "first,'some,text'" 6 {:cell "some,text" :next-index 17}
+
+      ;;Quoted to delimiter no escaped quotes
+      "'quoted, text',second" 0 {:cell "quoted, text" :next-index 14}
+
+      ;;Unquoted with escaped quotes to end
+      "first,escaped \\'quote" 6 {:cell "escaped 'quote" :next-index 21}
+
+      ;;Unquoted with escaped quotes to delimiter
+      "escaped \\'quotes\\',second" 0 {:cell "escaped 'quotes'" :next-index 18}
+
+      ;;Quoted with escaped quotes to end
+      "first,'\\'quoted\\' text'" 6 {:cell "'quoted' text" :next-index 23}
+
+      ;;Quoted with escaped quotes to delimiter
+      "'\\'quoted\\' text',second" 0 {:cell "'quoted' text" :next-index 17}
+
+      ;;Unquoted with escaped quotes to end
+      "first,\\'\\'" 6 {:cell "''" :next-index 10}
+
+      ;;Unquoted with escaped quotes to delimiter
+      "\\'\\',second" 0 {:cell "''" :next-index 4}
+
+      ;;Returns raw cell text
+      "  first  ,second" 0 {:cell "  first  " :next-index 9}
+
+      ;;empty cell at end of line
+      "a,b," 4 {:cell "" :next-index 4}))
+
+  (testing "Unclosed quote"
+    (is (thrown? IllegalArgumentException (parse-cell-escape "'Unclosed, quote :(" 1 0 {:quoteChar \' :escapeChar \\ :delimiter \,}))))
+
+  (testing "Dangling escape character"
+    (is (thrown? IllegalArgumentException (parse-cell-escape "Dangling escape\\" 1 0 {:quoteChar \' :escapeChar \\ :delimiter \,}))))
+
+  (testing "Non-delimiter character after closing quote"
+    (is (thrown? IllegalArgumentException (parse-cell-escape "'Quoted text' followed by more stuff" 1 0 {:quoteChar \' :escapeChar \\ :delimiter \,})))))
+
 (deftest parse-row-cells-test
   (testing "Quote and escape characters differ"
     (let [opts {:escapeChar \\ :quoteChar \" :delimiter \, :trim-mode :all}]
@@ -56,7 +150,22 @@
         (is (= ["a,b" "c" "d"] (parse-row-cells "\"a,b\",c,d" 0 opts))))
 
       (testing "Escaped quote"
-        (is (= ["a\"b" "c" "d"] (parse-row-cells "a\"\"b,c,d" 0 opts)))))))
+        (is (= ["a\"b" "c" "d"] (parse-row-cells "a\"\"b,c,d" 0 opts))))
+
+      (testing "Escaped quote at start of cell"
+        (is (= ["\"Quoted text\"" "b" "c"] (parse-row-cells "\"\"Quoted text\"\",b,c" 0 opts))))
+
+      (testing "Triple quotes"
+        (is (= ["\"Quoted text\", and more" "" "c"] (parse-row-cells "\"\"\"Quoted text\"\", and more\",,c" 0 opts))))))
+
+  (testing "Last cell empty"
+    (is (= ["a" "b" ""] (parse-row-cells "a,b," 1 {:escapeChar \" :quoteChar \" :delimiter \, :trim-mode :none}))))
+
+  (testing "Empty"
+    (is (= [] (parse-row-cells "" 1 {:escapeChar \" :quoteChar \" :delimiter \, :trim-mode :none}))))
+
+  (testing "Whitespace"
+    (is (= ["   "] (parse-row-cells "   " 1 {:escapeChar \" :quoteChar \" :delimiter \, :trim-mode :none})))))
 
 (deftest trim-cell-test
   (are [expected input mode] (= expected (trim-cell input mode))
