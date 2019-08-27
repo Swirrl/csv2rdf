a436320e4405f01df0b96277baf41603a23fe5a4
lkitching <lee@swirrl.com>
2019-08-27 17:55:43 +0100

    Update grafter version and add explicit sesame dependency.
    Issue #25 - Update grafter dependency to 2.0.3. This version depends
    on RDF4j instead of Sesame but still contains the legacy grafter.*
    namespaces. Add an explicit Sesame dependency and exclude the RDF4j
    dependencies.
    
    Add the test resources path to the deps test alias.
    
    Update version to 0.3.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swirrl/csv2rdf "0.2.8"
+(defproject swirrl/csv2rdf "0.3.0"
   :description "Library for converting CSV to RDF"
   :url "https://github.com/Swirrl/csv2rdf"
   :license {:name "Eclipse Public License"
