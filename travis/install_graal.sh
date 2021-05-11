3ae5326774144eb9941fdb6adf01b2fc1b4a4311
Robin Gower <robin.git@infonomics.ltd.uk>
2021-05-11 11:00:15 +0200

    Add install_graal.sh

---
+++ travis/install_graal.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+# Install GraalVM
+# https://www.graalvm.org/docs/getting-started/linux/
+
+sudo mkdir /opt/graal
+cd /opt/graal
+sudo curl -L -O https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-21.1.0/graalvm-ce-java8-linux-amd64-21.1.0.tar.gz
+sudo tar -xzf graalvm-ce-java8-linux-amd64-21.1.0.tar.gz
+
+export PATH=/opt/graal/graalvm-ce-java8-21.1.0/bin:$PATH
+export JAVA_HOME=/opt/graal/graalvm-ce-java8-21.1.0
+
+# Install native-image extension
+# https://www.graalvm.org/reference-manual/native-image/
+
+sudo apt install build-essential libz-dev zlib1g-dev
+sudo env JAVA_HOME=/opt/graal/graalvm-ce-java8-21.1.0 /opt/graal/graalvm-ce-java8-21.1.0/bin/gu install native-image
