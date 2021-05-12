d21fff040f107fee721e7b3cad9f1f1ff9b9052c
Rick Moynihan <rick.m@swirrl.com>
2021-05-12 17:59:12 +0100

    Version 0.4.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swirrl/csv2rdf "0.4.6"
+(defproject swirrl/csv2rdf "0.4.7-SNAPSHOT"
   :description "Library for converting CSV to RDF"
   :url "https://github.com/Swirrl/csv2rdf"
   :license {:name "Eclipse Public License"
