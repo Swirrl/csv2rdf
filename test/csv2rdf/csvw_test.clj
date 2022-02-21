25929b2c4750162cff1b1f9d60090df20a5f3875
Rick Moynihan <rick.m@swirrl.com>
2022-02-21 11:09:13 +0000

    Add tests for all supported types to csvw/csv->rdf

--- test/csv2rdf/csvw_test.clj
+++ test/csv2rdf/csvw_test.clj
@@ -1,15 +1,44 @@
 (ns csv2rdf.csvw-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest is testing]]
             [clojure.java.io :as io]
             [csv2rdf.csvw :as csvw])
   (:import [java.net URI]
            [java.nio.file FileSystems]
            [grafter_2.rdf.protocols Quad]))
 
-(defn- make-path [& path]
-  (.getPath (FileSystems/getDefault) "w3c-csvw" (into-array String path)))
+(def w3c-dir "w3c-csvw")
 
-(deftest csv->rdf-with-paths
-  (is (instance? Quad
-                 (first (csvw/csv->rdf (make-path "tests" "test011" "tree-ops.csv")
-                                       (make-path "tests" "test011" "tree-ops.csv-metadata.json"))))))
+(defn- make-path [& path]
+  (.getPath (FileSystems/getDefault) w3c-dir (into-array String path)))
+
+(defn make-file [& path]
+  (apply io/file w3c-dir path))
+
+(defn csv->rdf? [csv metadata]
+  (instance? Quad
+             (first (csvw/csv->rdf csv metadata))))
+
+(deftest csv->rdf-supported-types-test
+  (testing "with java.io.File"
+    (let [csv      (make-file "./tests" "test011" "tree-ops.csv")
+          metadata (make-file "./tests" "test011" "tree-ops.csv-metadata.json")]
+
+      (is (csv->rdf? csv metadata))
+      (is (csv->rdf? nil metadata)
+          "Resolves csv")))
+
+  (testing "with URI"
+    (let [csv      (.toURI (make-file "./tests" "test011" "tree-ops.csv"))
+          metadata (.toURI (make-file "./tests" "test011" "tree-ops.csv-metadata.json"))]
+
+      (is (csv->rdf? csv metadata))
+      (is (csv->rdf? nil metadata)
+          "Resolves csv")))
+
+  (testing "with java.nio.Path"
+    (let [csv      (make-path "tests" "test011" "tree-ops.csv")
+          metadata (make-path "tests" "test011" "tree-ops.csv-metadata.json")]
+
+      (is (csv->rdf? csv metadata))
+      (is (csv->rdf? nil metadata)
+          "Resolves csv"))))
