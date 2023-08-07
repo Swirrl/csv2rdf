eac8c4bc23b885b491def0c9f65c578efb2370a5
lkitching <lee@swirrl.com>
2023-08-07 10:52:35 +0100

    Add docker build task

--- build.clj
+++ build.clj
@@ -1,7 +1,9 @@
 (ns build
   (:require [clojure.tools.build.api :as b]
             [org.corfield.build :as bb]
-            [clojure.string :as str])
+            [clojure.string :as str]
+            [juxt.pack.api :as pack])
+  (:import [com.google.cloud.tools.jib.api JibContainer])
   (:refer-clojure :exclude [test]))
 
 (def lib 'swirrl/csv2rdf)
@@ -63,3 +65,43 @@
               :class-dir class-dir
               :uber-file "target/csv2rdf-app.jar"
               })))
+
+;; A tag name must be valid ASCII and may contain lowercase and uppercase
+;; letters, digits, underscores, periods and dashes
+;; https://docs.docker.com/engine/reference/commandline/tag/#extended-description
+(defn tag [s]
+  (when s (str/replace s #"[^a-zA-Z0-9_.-]" "_")))
+
+(def repo "europe-west2-docker.pkg.dev/swirrl-devops-infrastructure-1/public")
+
+(defn docker [opts]
+  (let [tags (->> ["rev-parse HEAD"
+                   "describe --tags --always"
+                   "branch --show-current"]
+                  (map #(tag (b/git-process {:git-args %})))
+                  (remove nil?))
+        image-type (get opts :image-type :docker)
+        build-args {:basis (b/create-basis {:project "deps.edn" :aliases [:docker]})
+                    ;; If we don't include a tag in the :image-name, then pack implicitly
+                    ;; tags the image with latest, even when we specify additional tags. So
+                    ;; choose a tag arbitrarily to be part of the :image-name, and then
+                    ;; provide the rest in :tags.
+                    :image-name (str repo "/csv2rdf:" (first tags))
+                    :tags (set (rest tags))
+                    :image-type image-type
+                    :base-image "eclipse-temurin:17" ;; An openJDK 17 base docker provided by https://github.com/adoptium/containers#containers
+                    }
+        publish-args {:platforms #{:linux/amd64 :linux/arm64}
+
+                      ;; NOTE Not as documented!
+                      ;; The docstring states that these should be
+                      ;;     :to-registry {:username ... :password ...}
+                      ;; but alas, that is a lie.
+                      ;; https://github.com/juxt/pack.alpha/issues/101
+                      :to-registry-username "_json_key"
+                      :to-registry-password (System/getenv "GCLOUD_SERVICE_KEY")}
+        args (if (= :remote (:to opts))
+               (merge build-args publish-args)
+               build-args)
+        ^JibContainer container (pack/docker args)]
+    (println (.. container (getDigest) (getHash)))))
